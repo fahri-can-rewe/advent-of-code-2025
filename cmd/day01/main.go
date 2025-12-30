818fa7b9cbeaa1f830318f5962658977ad81b5a1
Fahri Can <f.can@rewe-group.at>
2025-12-30 21:04:49 +0100

    init: Rename methods.

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -12,6 +12,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("The password for Part1 is: ", moveDialP1(generateInstructions(input)))
-	fmt.Println("The password for Part2 is: ", moveDialP2(generateInstructions(input)))
+	fmt.Println("The password for Part1 is: ", countDialPointsZero(generateInstructions(input)))
+	fmt.Println("The password for Part2 is: ", countDialPointsAndPassedZero(generateInstructions(input)))
 }
