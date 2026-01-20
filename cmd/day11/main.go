79ab33bcc273cd0e4ed00a1a270c638cf4429ef5
Fahri Can <f.can@rewe-group.at>
2026-01-20 22:49:26 +0100

    chore: Finish Part 2.

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -8,11 +8,19 @@ import (
 )
 
 func main() {
-	input, err := util.ReadInput("cmd/day11/input1.txt")
+	input1, err := util.ReadInput("cmd/day11/input1.txt")
+	input2, err := util.ReadInput("cmd/day11/input2.txt")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	racks := parseInput(input)
-	steps := useDFS(racks)
-	fmt.Println("Part 1 amount different paths to out:", steps)
+
+	racksP1 := parseInput(input1)
+	racksP2 := parseInput(input2)
+
+	p1 := useDFS(racksP1, false)
+	p2 := useDFS(racksP2, true)
+
+	fmt.Println("Part 1 amount different paths to out:", p1)
+	fmt.Println("Part 2 amount paths visit dac and fft:", p2)
 }
--- cmd/day11/reactor.go
+++ cmd/day11/reactor.go
@@ -5,6 +5,12 @@ import "strings"
 const start = "you"
 const end = "out"
 
+type State struct {
+	current string
+	hasDac  bool
+	hasFft  bool
+}
+
 func parseInput(input string) map[string][]string {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
 	racks := make(map[string][]string)
@@ -49,7 +55,41 @@ func countPaths(racks map[string][]string, current string, memo map[string]int) 
 	return totalPaths
 }
 
-func useDFS(racks map[string][]string) int {
+func useDFS(racks map[string][]string, isPartTwo bool) int {
 	memo := make(map[string]int)
+	if isPartTwo {
+		return countPathsWithTargets(racks, "svr", false, false, make(map[State]int))
+	}
 	return countPaths(racks, start, memo)
+}
+
+func countPathsWithTargets(racks map[string][]string, current string, hasDac, hasFft bool, memo map[State]int) int {
+	// Update state if we just arrived at a target
+	if current == "dac" {
+		hasDac = true
+	}
+	if current == "fft" {
+		hasFft = true
+	}
+
+	// Base Case
+	if current == "out" {
+		if hasDac && hasFft {
+			return 1
+		}
+		return 0
+	}
+
+	state := State{current, hasDac, hasFft}
+	if val, ok := memo[state]; ok {
+		return val
+	}
+
+	total := 0
+	for _, neighbor := range racks[current] {
+		total += countPathsWithTargets(racks, neighbor, hasDac, hasFft, memo)
+	}
+
+	memo[state] = total
+	return total
 }
